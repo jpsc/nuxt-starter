3ee22725708ca9e131a16178e6dabb162278dc2e
João Carmona <joao@funda.nl>
2018-06-08 23:19:10 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Usage
+
+## Development
+
+```bash
+# serve with hot reloading at localhost:3333
+$ npm run dev
+```
+
+## Production
+
+```bash
+# build for production and launch the server
+$ npm run build # Or yarn build
+$ npm start # Or yarn start
+```
---
+++ template/nuxt.config.js
@@ -0,0 +1,66 @@
+const tailwind = require('tailwindcss');
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+const path = require('path')
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    titleTemplate: '%s | {{ name }}',
+    htmlAttrs: { lang: 'en' },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '{{escape description }}' }
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: "#004066" },
+  /*
+  ** Build configuration
+  */
+  build: {
+    extractCSS: true,
+    postcss: [
+      tailwind('./tailwind.js'),
+      require('cssnano')({
+        preset: 'default',
+        discardComments: { removeAll: true }
+      })
+    ],
+    extend (config, { isDev, isClient }) {
+      /*
+      ** Cleanup CSS with PurgeCSS
+      */
+      if (!isDev) {
+        config.plugins.push(
+          new PurgecssPlugin({
+            paths: glob.sync([
+              path.join(__dirname, './pages/**/*.vue'),
+              path.join(__dirname, './layouts/**/*.vue'),
+              path.join(__dirname, './components/**/*.vue')
+            ]),
+            whitelist: ['html', 'body']
+          })
+        )
+      }
+    }
+  },
+  css: [
+    '@/assets/css/main.css'
+  ],
+  router: {
+  },
+  modules: [
+    ['@nuxtjs/pwa', { onesignal: false }],
+    '@nuxtjs/axios',
+    '@nuxtjs/auth'
+  ],
+};
